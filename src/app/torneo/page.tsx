e748f367926ccb2b98d8039b6f589b6f0ae12a5e
NovoxMx <A00835132@tec.mx>
2025-08-06 03:38:26 -0700

    Ajustar el ancho del contenedor en las páginas de torneo y tabla

--- src/app/torneo/page.tsx
+++ src/app/torneo/page.tsx
@@ -7,7 +7,7 @@ export default function TorneoPage() {
         <div className='bg-[#000000] text-[#e0e0e0] min-h-screen font-sans'>
             <div className="bg-[#121212] text-white min-h-screen px-4 py-6 max-w-4xl mx-auto">
                 <h1 className="text-3xl text-[#33aaff] font-bold mb-6">Torneo Eliminatorio</h1>
-                 <div className="rounded-md bg-white w-1/8 md:w-2/8 flex justify-center item-center hover:bg-transparent hover:border-1 hover:border-[#33aaff]">
+                 <div className="rounded-md bg-white w-2/8 lg:w-1/8 flex justify-center item-center hover:bg-transparent hover:border-1 hover:border-[#33aaff]">
                     <Link href="/" className="flex justify-center text-[#33aaff] ">Ver Tabla </Link>
                 </div>
                 <br/>
